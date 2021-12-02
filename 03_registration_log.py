ef3c941114d6a2997ac5ab69963e5990a6954832
Bondarenko <Homja4iwe@gmail.com>
2021-12-02 21:00:01 +0300

    Выполнил задание №3

--- 03_registration_log.py
+++ 03_registration_log.py
@@ -22,25 +22,52 @@
 # - поле возраст НЕ является числом от 10 до 99: ValueError
 # Вызов метода обернуть в try-except.
 
-def registration(line):
-    if len(line.split()) == 3:
-        name, email, age = line.split(' ')
-        if name.isalpha() and age.isdigit():
-            if '@ ' and '.' in email and 10 <= int(age) <= 99:
-                with open('registrations_good.log', mode='a') as file:
-                    file.write(f'{line}\n')
-            else:
-                with open('registrations_bad.log', mode='a') as file:
-                    file.write(f'{line}\n')
+class NotNameError(Exception):
+    pass
+
+
+class NotEmailError(Exception):
+    pass
+
+
+class Registration:
+
+    def __init__(self, file, file_registrations_bad, file_registrations_good):
+        self.file = file
+        self.file_registrations_bad = file_registrations_bad
+        self.file_registrations_good = file_registrations_good
+
+    def registration_error(self, line):
+        if len(line.split()) == 3:
+            name, email, age = line.split(' ')
+            if not name.isalpha():
+                raise NotNameError('поле имени содержит Не только буквы!')
+            if not 9 < int(age) < 99 or not age.isdigit():
+                raise ValueError('поле возраст НЕ является числом от 10 до 99!')
+            if ('@ ' and '.') not in email:
+                raise NotEmailError('поле емейл НЕ содержит @ и .(точку)!')
         else:
-            with open('registrations_bad.log', mode='a') as file:
-                file.write(f'{line}\n')
-    else:
-        with open('registrations_bad.log', mode='a') as file:
-            file.write(f'{line}\n')
+            raise ValueError('НЕ присутсвуют все три поля!')
+
+    def registration(self):
+        with open(self.file, 'r') as ff:
+            for line in ff:
+                line = line[:-1]
+                try:
+                    self.registration_error(line)
+                    with open(self.file_registrations_good, mode='a', encoding='utf-8') as file:
+                        file.write(line + '\n')
+                    continue
+                except ValueError as exc:
+                    print(line, exc)
+                except NotNameError as exc:
+                    print(line, exc)
+                except NotEmailError as exc:
+                    print(line, exc)
+                with open(self.file_registrations_bad, mode='a', encoding='utf-8') as file:
+                    file.write(line + '\n')
 
 
-with open('registrations.txt', 'r') as ff:
-    for line in ff:
-        line = line[:-1]
-        registration(line=line)
+check = Registration(file='registrations.txt', file_registrations_bad='registrations_bad.log',
+                     file_registrations_good='registrations_good.log')
+check.registration()
